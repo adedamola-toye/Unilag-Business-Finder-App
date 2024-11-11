7db43990706c7f6aed8edbcd72fbe457a984920a
adedamola-toye <toye.adedamola@gmail.com>
2024-11-11 15:55:42 +0100

    sign up in nav bar now working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,70 +1,66 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useContext } from "react";
-import ModalContext from "../contexts/ModalContext";
-import './custom.css';
+import "./custom.css";
 import { useSelector, useDispatch } from "react-redux";
-import { customSignOut } from "../redux/features/auth/authService"
+import { customSignOut } from "../redux/features/auth/authService";
 import { setUser } from "../redux/features/auth/authSlice";
+import { openModal } from "../redux/features/modal/modalSlice";
 
 export default function NavBar() {
-    const {openModal}= useContext(ModalContext)
-    const user = useSelector((state)=>state.auth.user)
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth.user);
+  const navigate = useNavigate();
 
-    const openSignUp = (event) => {
-        event.preventDefault();
-        openModal("signup")
-    }
-    const openLogin = (event) => {
-        event.preventDefault();
-        console.log("Opening Login Modal");
-        openModal("login");
-    };
+  const openSignUp = (event) => {
+    event.preventDefault();
+    dispatch(openModal("signup"));
+  };
+  const openLogin = (event) => {
+    event.preventDefault();
+    console.log("Opening Login Modal");
+    dispatch(openModal("login"));
+  };
 
-    //Handle sign out
-    const handleSignOut =async () =>{
-        try{
-            await customSignOut();
-            dispatch(setUser(null))
-            navigate("/")
-        }
-        catch(error){
-            console.log("Error signing out: ", error)
-        }
+  //Handle sign out
+  const handleSignOut = async () => {
+    try {
+      await customSignOut();
+      dispatch(setUser(null));
+      navigate("/");
+    } catch (error) {
+      console.log("Error signing out: ", error);
     }
-    
-    return (
-        <nav className="uppercase">
-            <ul className="flex list-none space-x-[35px] text-[16px] lg:text-[16px] cus">
-                <li>
-                    <Link
-                        to="/"
-                        className="decoration-black hover:bg-complementary rounded-sm py-3 px-5 xl:px-5 transition-all duration-300 ease-in-out transform hover:scale-105"
-                    >
-                        Home
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        to="/explore-business"
-                        className="decoration-black hover:bg-complementary rounded-sm py-3 px-5 transition-all duration-300 ease-in-out transform hover:scale-105"
-                    >
-                        Explore Businesses
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        to="/blog"
-                        className="decoration-black hover:bg-complementary rounded-sm py-3 px-5 transition-all duration-300 ease-in-out transform hover:scale-105"
-                    >
-                        Blog
-                    </Link>
-                </li>
-                
-                
-                {/* i dont get why this didn't work */}
-                {/* <li>
+  };
+
+  return (
+    <nav className="uppercase">
+      <ul className="flex list-none space-x-[35px] text-[16px] lg:text-[16px] cus">
+        <li>
+          <Link
+            to="/"
+            className="decoration-black hover:bg-complementary rounded-sm py-3 px-5 xl:px-5 transition-all duration-300 ease-in-out transform hover:scale-105"
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/explore-business"
+            className="decoration-black hover:bg-complementary rounded-sm py-3 px-5 transition-all duration-300 ease-in-out transform hover:scale-105"
+          >
+            Explore Businesses
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/blog"
+            className="decoration-black hover:bg-complementary rounded-sm py-3 px-5 transition-all duration-300 ease-in-out transform hover:scale-105"
+          >
+            Blog
+          </Link>
+        </li>
+
+        {/* i dont get why this didn't work */}
+        {/* <li>
                     <Link
                         to="/hire-a-professional"
                         className="decoration-black hover:bg-complementary rounded-sm py-3 px-5 transition-all duration-300 ease-in-out transform hover:scale-105"
@@ -81,35 +77,38 @@ export default function NavBar() {
                     </Link>
                 </li> */}
 
-                
-                {user ? (
-                    <li>
-                        <Link className="decoration-black bg-complementary p-3 px-10 rounded hover:bg-main transition-all duration-300 ease-in-out transform hover:scale-105" onClick={handleSignOut}>Log out</Link>
-                    </li>
-                ): (
-                    <>
-                        <li>
-                    <Link
-                        to="/signup"
-                        onClick={openSignUp}
-                        className="decoration-black bg-complementary p-3 px-10 rounded hover:bg-main transition-all duration-300 ease-in-out transform hover:scale-105"
-                    >
-                        Sign Up
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        to="/login"
-                        onClick={openLogin}
-                        className="decoration-black p-3 px-10 rounded border-4 border-complementary hover:bg-complementary transition-all duration-300 ease-in-out transform hover:scale-105"
-                    >
-                        Log In
-                    </Link>
-                </li>
-                    </>
-                )}
-                
-            </ul>
-        </nav>
-    );
+        {user ? (
+          <li>
+            <Link
+              className="decoration-black bg-complementary p-3 px-10 rounded hover:bg-main transition-all duration-300 ease-in-out transform hover:scale-105"
+              onClick={handleSignOut}
+            >
+              Log out
+            </Link>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link
+                to="/signup"
+                onClick={openSignUp}
+                className="decoration-black bg-complementary p-3 px-10 rounded hover:bg-main transition-all duration-300 ease-in-out transform hover:scale-105"
+              >
+                Sign Up
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/login"
+                onClick={openLogin}
+                className="decoration-black p-3 px-10 rounded border-4 border-complementary hover:bg-complementary transition-all duration-300 ease-in-out transform hover:scale-105"
+              >
+                Log In
+              </Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
 }
